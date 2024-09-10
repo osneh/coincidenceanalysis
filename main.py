2569a3b3b0f46ee1eeb3876884ecd4588a702d97
habreu <h.abreu@ip2i.in2p3.fr>
2024-09-10 14:06:04 +0200

    setting files path and logging handlers

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import logging
+
+from logging.handlers import RotatingFileHandler
+
+CWD = os.path.sep.join(os.path.abspath(__file__).split(os.path.sep)[:-1])
+Data = os.path.sep.join([CWD,'data'])
+Src = os.path.sep.join([CWD,'src'])
+Logs = os.path.sep.join([CWD,'logs'])
+LogFile = os.path.sep.join([Logs,'App.log'])
+
+logging.basicConfig(format = "%(asctime)s %(levelname)s :: %(message)s", level=logging.DEBUG)
+##logging.basicConfig(format = "%{asctime} %{levelname} :: %{message}", level=logging.DEBUG)
+logger = logging.getLogger('Analysis')
+handler = RotatingFileHandler(LogFile, maxBytes=1000000, backupCount=100, encoding='utf-8',delay=0)
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
+
+##print(CWD)
+##print(Data)
+##print(Src)
+
+for folder in [Data, Src]:
+    if os.path.isdir(folder):
+        logger.info(f"Folder is good: {folder}")
+    else :
+        print("Missing folder:, aborting execution!")
